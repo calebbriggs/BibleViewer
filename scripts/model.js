5b5d71fbd988625470dd5968ae8473ca93801350
Caleb Briggs <caleb@ninecollective.com>
2012-03-09 08:16:04 -0600

    moving to model

---
+++ scripts/model.js
@@ -0,0 +1,30 @@
+var model = function(){
+  				var _self =this;
+
+  				bible.name = "ASV";
+  				KJV.name = "KJV";
+
+  				this.bibles = [bible, KJV]
+  				this.currentBible = ko.observable();
+	  			this.books =ko.observableArray([]);
+	  			this.chapters = ko.observableArray([]);
+	  			this.verses = ko.observableArray([]);
+	  			this.currentBook = ko.observable();
+	  			this.currentChapter = ko.observable();
+	  			this.currentVerse = ko.observable();
+
+	  			this.currentBible.subscribe(function(newValue){
+  					_self.books(newValue.books);
+	  			});
+	  			this.currentBook.subscribe(function(newValue){
+  					_self.chapters(newValue.chapters);
+	  			});
+	  			this.currentChapter.subscribe(function(newValue){
+  					// _self.verses(newValue.verses);
+  					var chapter = ""
+  					_.each(newValue.verses, function(Verse){ chapter += " "+ Verse.number + " "+ Verse.text });
+
+  					_self.currentVerse(chapter);
+	  			});
+
+	  		};
