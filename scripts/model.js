0de873d4b40a1b5529406e608455e16f7de4c1dd
Caleb Briggs <caleb@ninecollective.com>
2012-03-20 22:42:43 -0500

    merging

--- scripts/model.js
+++ scripts/model.js
@@ -1,6 +1,5 @@
 var model = function(books){
-  				var _self =this;				
-  				
+  				var _self =this;		
 
   				this.bibles = ["ASV", "KJV"];
   				this.currentBible = ko.observable();
@@ -12,61 +11,17 @@ var model = function(books){
 	  			this.currentBookNumber = ko.observable();
 	  			this.currentChapterNumber = ko.observable();
 
-
 	  			this.currentBible.subscribe(function (newValue) {
 	  				if (newValue && _self.currentBook()) {
-
-	  					var postData = {currentBible: _self.currentBible(), currentBook: _self.currentBook().Book};
-
-	  					$.ajax({
-			                url: '/',
-			                async: true,
-			                data: JSON.stringify(postData) ,
-			                type: "Post",
-			                contentType: 'application/json',
-			                success: function(result){			                	
-			                	_self.chapters(result.chapters);
-			                	
-								if(_self.currentChapterNumber())
-									_self.currentChapter(result.chapters[_self.currentChapterNumber()]);	
-								else
-									_self.currentChapter(result.chapters[0]);
-
-								if(_self.currentChapterNumber())
-									_self.currentChapter(result.chapters[_self.currentChapterNumber()]);					
-				                }
-				            });
-
-	  				};
-	  				
-	  			})
+	  					getBookFromBible();
+	  				}
+	  			});
 	  		
 	  			this.currentBook.subscribe(function(newValue){
 	  				if(newValue)
 	  				{
-	  					var postData = {currentBible: _self.currentBible(), currentBook: newValue.Book};
-
-	  					$.ajax({
-			                url: '/',
-			                async: true,
-			                data: JSON.stringify(postData) ,
-			                type: "Post",
-			                contentType: 'application/json',
-			                success: function(result){			                	
-			                	_self.chapters(result.chapters);
-			                	
-								if(_self.currentChapterNumber())
-									_self.currentChapter(result.chapters[_self.currentChapterNumber()]);	
-								else
-									_self.currentChapter(result.chapters[0]);
-
-								if(_self.currentChapterNumber())
-									_self.currentChapter(result.chapters[_self.currentChapterNumber()]);					
-				                }
-				            });
-
-	  					_self.currentBookNumber(newValue.Number-1);
-	  						
+	  					getBookFromBible();
+	  					_self.currentBookNumber(newValue.Number-1);	  						
 	  				}
   					
 	  			});
@@ -75,7 +30,28 @@ var model = function(books){
 	  				if(newValue){
 	  					_self.currentChapterNumber(newValue.number-1);
 	  				}
-	  			})
-	  			
+	  			});
+
+	  			var getBookFromBible = function(){
+	  				var postData = {currentBible: _self.currentBible(), currentBook: _self.currentBook().Book};
+  					$.ajax({
+		                url: '/',
+		                async: true,
+		                data: JSON.stringify(postData) ,
+		                type: "Post",
+		                contentType: 'application/json',
+		                success: function(result){			                	
+		                	_self.chapters(result.chapters);
+		                	
+							if(_self.currentChapterNumber())
+								_self.currentChapter(result.chapters[_self.currentChapterNumber()]);	
+							else
+								_self.currentChapter(result.chapters[0]);
+
+							if(_self.currentChapterNumber())
+								_self.currentChapter(result.chapters[_self.currentChapterNumber()]);					
+			                }
+		            });	  				
+	  			};  			
 
 	  		};
