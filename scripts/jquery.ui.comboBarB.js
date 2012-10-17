09eb2c4cd550160801fd43413363c87a8b9bd6c1
Caleb Briggs <caleb@ninecollective.com>
2012-10-17 11:40:59 -0500

    made comboBarB sortable filterable

--- scripts/jquery.ui.comboBarB.js
+++ scripts/jquery.ui.comboBarB.js
@@ -8,7 +8,8 @@
 				datasource: [],
 				valueObject: 'value',
 				displayNameObject: 'displayName',
-				value: {}
+				value: {},
+				spans: []
 				
         },
         _create: function () {
@@ -16,35 +17,88 @@
 				o = self.options,
 				$el = self.element;
 				
-				o._datasource = o.datasource;
+			self._index = 0;
+			self._maxindex = o.datasource.length;
 				
-				$el.addClass("ui-widget-border");
+			o._datasource = o.datasource;
 				
-				this._input = $('<input class="ui-comboBarB-input">').val("Choose...").width($el.width()).appendTo($el);
+			this._input = $('<input class="ui-comboBarB-input">').width($el.width()).appendTo($el);
 				
             this._optionsDiv = $('<div></div>')
                 .addClass('ui-comboBarB-div')
 				.width($el.width())
                 .hide()
                 .appendTo($el);
-            this.element
-                 .bind('mouseenter', $.proxy(this._open, this))
-                .bind('mouseleave', $.proxy(this._close, this));
+            this._input
+               	.bind('focus', $.proxy(this._open, this));
+                //.bind('blur', $.proxy(this._close, this));
 				
 			this._input
-				.bind('keyup', $.proxy(this._search, this));
+				.bind('keyup', $.proxy(this._search, this));				
 			
         },
 		
 		_init: function() {
 			this.refresh();
 		},
-		
-		_search: function(){
+		select: function(keycode){
+				var self = this,
+				o = self.options,
+				$el = self.element;				
+				self._open();
+				
+				if(keycode == 40 ){
+					
+					if(self._index ==  (self._maxindex -1)){
+							self._index = 0;
+					}
+					else{
+						self._index++;
+					}
+					var selectedSpan = o.spans[self._index];
+					
+					if(selectedSpan){
+						selectedSpan.addClass("ui-state-selected");
+					}				
+				}
+				if(keycode == 38 ){
+					if(self._index == 0){
+						self._index = self._maxindex -1;
+					}
+					else{
+						self._index--;
+					}
+					var selectedSpan = o.spans[self._index];
+					
+					if(selectedSpan){
+						selectedSpan.addClass("ui-state-selected");
+					}
+					
+				}
+				
+		 },
+		_search: function(e){
 			var self = this,
 				o = self.options,
 				$el = self.element;
-											
+								
+				if(e.keyCode == 40 || e.keyCode == 38){
+						self.select(e.keyCode);
+						return false;
+				}
+				
+				if(e.keyCode == 13){				
+					var selectedSpan = o.spans[self._index];
+					if(selectedSpan){
+						selectedSpan.click();
+					}
+					return false;
+				}
+				if(e.keycode == 9){	
+					self._close();
+					return false;
+				}
+										
 				o.datasource = _.filter(o._datasource,function(data){
 					var dis = typeof data=="function" ? data() : data;
 					if(typeof dis == "object"){
@@ -52,8 +106,11 @@
 					}					
 					return dis.contains(self._input.val());				
 				});
+				self._maxindex = o.datasource.length;
+				self._index = 0;
 				
 				self._open();
+				
 		},
 
         _open: function () {
@@ -61,6 +118,7 @@
 				o = self.options,
 				$el = self.element;
 			var div = $('<div>');
+			var spans = [];
 			 _.each(o.datasource,function(data){
 				var display =data[o.displayNameObject] || data;
 				if(typeof display =="function"){display = display();}
@@ -86,6 +144,7 @@
 							o.value=val;
 						}
 					}
+					
 					self._input.val(display);
 					self._close();
 				}				
@@ -99,14 +158,16 @@
 				if(dis == display){
 					span.addClass('ui-state-selected');
 				}
+				spans.push(span);
 				div.append(span);
 			});
+			o.spans=spans;
 			this._optionsDiv.html(div);
 
             this._optionsDiv.show();
         },
         _close: function () {
-            this._optionsDiv.hide();
+			this._optionsDiv.hide();            
         },
 		
 		refresh: function(){
@@ -115,13 +176,15 @@
 				$el = self.element;
 			
 			o._datasource = o.datasource;
+			self._maxindex = o.datasource.length;
+			self._index = 0;
 			var val = typeof o.value =="function" ? o.value() : o.value;
 			
 			if(typeof val == "object"){
-				self._input.val((val[o.displayNameObject] || "Choose ..."));
+				self._input.val((val[o.displayNameObject] || ""));
 			}
 			else{
-				self._input.val((val || "Choose ..."));
+				self._input.val((val || ""));
 			}
 		},
 
